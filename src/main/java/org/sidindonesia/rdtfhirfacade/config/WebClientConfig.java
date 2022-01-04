07d2cbff6dc8fd0af2adeb56a3d68bc6aa3c83cd
Levi <muhammad.levi@sid-indonesia.org>
2022-01-04 15:08:28 +0700

    Affected Issue(s): https://www.notion.so/sid-indonesia/Back-End-Create-FHIR-Facade-server-for-RDT-app-ecosystem-1ed5f60ebdf049c98fe2ed4d0231e36f
    What this commit has achieved:
    1. Test run as Spring Boot App (using embedded container, Tomcat)
    2. Fixed issue on Spring Boot 2.6.x for Keycloak Spring Boot Adapter by
    moving the `keycloakConfigResolver` to a new `@Configuration` class
    3. TODO configure WebClient to use OAuth 2.0 from integrated Keycloak

--- src/main/java/org/sidindonesia/rdtfhirfacade/config/WebClientConfig.java
+++ src/main/java/org/sidindonesia/rdtfhirfacade/config/WebClientConfig.java
@@ -1,6 +1,7 @@
 /* (C)2022 */
 package org.sidindonesia.rdtfhirfacade.config;
 
+import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,8 +10,23 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
+//	@Bean
+//	public WebClient openSRPServerClient(@Value("${opensrp-server.base-url}") String baseURL,
+//		OAuth2AuthorizedClientManager authorizedClientManager) {
+//		ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2Client =
+//            new ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
+//
+//
+//		return WebClient.builder().baseUrl(baseURL).apply(oauth2Client.oauth2Configuration()).build();
+//	}
 	@Bean
-	public WebClient openSRPServerClient(@Value("${opensrp-server.base-url}") String baseURL) {
-		return WebClient.create(baseURL);
+	public WebClient openSRPServerClient(ListableBeanFactory beanFactory,
+		@Value("${opensrp-server.base-url}") String baseURL) {
+
+		for (String beanDefinitionName : beanFactory.getBeanDefinitionNames()) {
+			System.out.println(beanDefinitionName);
+		}
+
+		return WebClient.builder().baseUrl(baseURL).build();
 	}
 }
